fa32380ff3e1a8963f372d5cd216361d2784a7dd
Ismama <gayeismael22@gmail.com>
2025-01-16 08:22:15 +0100

    add gitignore

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -2,7 +2,7 @@
 
 
 void kmain(void){
-    //clear(); 
+    clear(); 
 
     volatile char *vga_buf = (volatile char *)0xb8000;
 	const char *msg = "ABCDEFG";
@@ -17,13 +17,12 @@ void kmain(void){
     
     //Works
     *vga_buf = msg[0];
-	*(++vga_buf) = 0x07;
-
-    *(++vga_buf) = msg[1];
-	*(++vga_buf) = 0x07;
-
-    *(++vga_buf) = msg[2];
-	*(++vga_buf) = 0x07;
-    
+    *(vga_buf + 1) = 0x07;
+    *(vga_buf + 2) = msg[1];
+    *(vga_buf + 3) = 0x07;
+    *(vga_buf + 4) = msg[2];
+    *(vga_buf + 5) = 0x07;
+    *(vga_buf + 6) = msg[3];
+    *(vga_buf + 7) = 0x07;
     
 }
