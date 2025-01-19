71a5369302d20b5e49331d850dff138cd2d48376
Ismama <gayeismael22@gmail.com>
2025-01-19 17:53:58 +0100

    it seems to work

--- src/kernel/idt.cpp
+++ src/kernel/idt.cpp
@@ -6,7 +6,8 @@
 typedef struct {
     uint16 offset1;
     uint16 segment_selector;
-    uint16 ist;
+    uint8 ist;
+    uint8 flags;
     uint16 offset2;
     uint32 offset3;
     uint32 reserved;
@@ -15,7 +16,7 @@ typedef struct {
 
 typedef struct {
     uint16 size;
-    uint32 offset;
+    uint64 offset;
 }__attribute__((packed)) idtr_t;
 
 
@@ -24,25 +25,26 @@ static idt_entry_t idt[IDT_LENGTH];
 static idtr_t idtr;
 
 
+extern uint64 isr_table[];
+
 void fill_idt(void){
     for(int i = 0; i < IDT_LENGTH; i++){
-        idt[i].offset1 = 0;
-        idt[i].segment_selector = 0;
+        idt[i].offset1 = isr_table[i] & 0xFFFF;
+        idt[i].segment_selector = 0x08;
         idt[i].ist = 0;
-        idt[i].offset2 = 0;
-        idt[i].offset3 = 0;
+        idt[i].flags = 0b10001110;
+        idt[i].offset2 = (isr_table[i] >> 16) & 0xFFFF;
+        idt[i].offset3 = (isr_table[i] >> 32) & 0xFFFFFFFF;
         idt[i].reserved = 0;
     }
 }
 
-extern void* isr_table[32];
-
 void init_idt(void){
     fill_idt();
 
-    idtr.offset = 0;
-    idtr.size = sizeof(idt);
+    idtr.size = (uint16)(sizeof(idt_entry_t) * IDT_LENGTH - 1);
+    idtr.offset = (uint64)&idt[0];
 
-    //asm __volatile__("lidt");
-    //asm __volatile__("sti");
+    asm __volatile__("lidt %0" : : "m"(idtr));
+    asm __volatile__("sti");
 }
--- src/kernel/interrupts_handler.cpp
+++ src/kernel/interrupts_handler.cpp
@@ -3,8 +3,9 @@ extern "C" {
 __attribute__((noreturn))
 void interrupt_handler(void);
 
-}
 
 void interrupt_handler(){
     asm __volatile__("cli; hlt");
+}
+
 }
