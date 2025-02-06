7c217ac44aa0c7e67ce2a16e218e35d97c397474
Ismama <gayeismael22@gmail.com>
2025-02-06 13:17:48 +0100

    add error code handling in exception handler

--- src/kernel/exceptions_handler.cpp
+++ src/kernel/exceptions_handler.cpp
@@ -2,13 +2,17 @@
 
 extern "C" {
 
-void exception_handler(void){
+void exception_handler(uint32 error_code){
     asm __volatile__("cli; hlt");
 }
 
 void kernel_panic(const char msg[]){
     raw_print(msg, 0, 0, WHITE, RED);
     asm __volatile__("cli; hlt");
+}
+
+void kernel_warning(const char msg[]){
+    raw_print(msg, 0, 0, WHITE, YELLOW);
 }
 
 }
